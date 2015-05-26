5a5c554c13ac2dd2dfcde3e4dc7d441e5b2a51ac
Rob Wormald <robwormald@gmail.com>
2015-05-26 01:47:31 -0700

    make redirect uri configurable

--- config/github.js
+++ config/github.js
@@ -1,6 +1,6 @@
 module.exports.github = {
   authenticate_uri: 'https://github.com/login/oauth/authorize',
-  redirect_uri: 'http://localhost:1337/github/authenticate/callback',
+  redirect_uri: process.env.GITHUB_REDIRECT_URI || 'http://localhost:1337/github/authenticate/callback',
   token_uri: 'https://github.com/login/oauth/access_token',
   client_id: process.env.GITHUB_CLIENT_ID,
   client_secret: process.env.GITHUB_CLIENT_SECRET,
